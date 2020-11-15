4a3b2a585c2befd9503bb30136eb656e1cd7319e
dahoff7 <dahoff504@gmail.com>
2020-11-15 16:22:26 -0700

    Login page

--- client/project/src/App.js
+++ client/project/src/App.js
@@ -6,16 +6,18 @@ import Home from './Home'
 import Schedule from './Schedule';
 import Login from './Login';
 import Tasks from './Tasks';
+import Signup from './Signup';
 
 function App() {
   return (
     <div>
       <Nav/>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Login}/>
           <Route path="/schedule" component={Schedule}/>
-          <Route path="/login" component={Login}/>
+          <Route path="/home" component={Home}/>
           <Route path="/tasks" component={Tasks}/>
+          <Route path="/signup" component={Signup}/>
         </Switch>
         
     </div>
